881c4425646632a8a9da99c5bf75b6c72152a199
Burhan Semiz <77208168+burhansemiz2020@users.noreply.github.com>
2023-04-24 19:00:13 +0200

    Add files via upload

---
+++ day05_update.sql
@@ -0,0 +1,32 @@
+/* ====================================== UPDATE ========================================
+Tabloda varolan herhangi bir record'a ait verinin degistirilmesi icin kullanilir.
+-----Syntax-----
+UPDATE table_name
+SET field1=''
+WHERE condition;
+UPDATE islemlerinin yapilabilmesi icin Ayarlar -> SQL Editor -> "Safe Updates" check box'indaki
+tik isaretini kaldirip kaydetmemiz gerekiyor, sonrasinda MySQL'i kapatip tekrar acmaliyiz
+========================================================================================*/
+
+
+
+/*--------------------------------------------------------------------------------------
+1) Bir firmalar tablosu olusturun icinde id,
+isim ve iletisim_isim field'lari olsun. 
+---------------------------------------------------------------------------------------*/
+CREATE TABLE firmalar
+(
+id INT,
+isim VARCHAR(10),
+iletisim_isim VARCHAR(15)
+);
+/*--------------------------------------------------------------------------------------
+2) Icine 3 kayit ekleyin :
+(1, 'ACB', 'Ali Can'), 
+(2, 'RDB', 'Veli Gul'), 
+(3, 'KMN', 'Ayse Gulmez').
+---------------------------------------------------------------------------------------*/
+INSERT INTO firmalar VALUES 
+(1, 'ACB', 'Ali Can'), 
+(2, 'RDB', 'Veli Gul'), 
+(3, 'KMN', 'Ayse Gulmez');
